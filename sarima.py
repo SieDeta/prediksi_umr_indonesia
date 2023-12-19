aeb1b591645bf71d7547b91d1a6ff814a7580fb4
Deta Dirganjaya <123723930+SieDeta@users.noreply.github.com>
2023-12-19 11:14:14 +0700

    sarima.py

--- sarima.py
+++ sarima.py
@@ -43,6 +43,7 @@ def predict_salary_increase_sarima(df, region, future_years):
     # Menampilkan hasil dengan pembulatan
     st.write(f'Perbedaan prediksi nominal sekitar Rp {rounded_rmse},00 dari nominal asli')
     st.write(f'Tingkat error prediksi: {rounded_mape}%')
+    st.write(f'Tingkat akurasi prediksi: {100.00-rounded_mape}%')
 
     # Predict future values
     start_year = df_region['YEAR'].max() + 1
@@ -74,6 +75,7 @@ def predict_salary_increase_sarima_indonesia(df, future_years):
     # Menampilkan hasil dengan pembulatan
     st.write(f'Perbedaan prediksi nominal sekitar Rp {rounded_rmse},00 dari nominal asli')
     st.write(f'Tingkat error prediksi: {rounded_mape}%')
+    st.write(f'Tingkat akurasi prediksi: {100.00-rounded_mape}%')
     
     # Predict future values
     start_year = df_indonesia['YEAR'].max() + 1
@@ -87,52 +89,51 @@ def predict_salary_increase_sarima_indonesia(df, future_years):
 # Streamlit App
 st.title('Prediksi Gaji UMR Indonesia')
 
-# Input file CSV
-uploaded_file = st.file_uploader("Upload File CSV", type=["csv"])
+file_path = "Indonesian Salary by Region (1997-2022).csv"
+df = pd.read_csv(file_path)
 
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.write('Data yang Diunggah:')
-    st.write(df)
+# Show the uploaded data
+st.write('Data UMR Indonesia (1997-2022)')
+st.write(df)
 
-    # Input
-    selected_region = st.selectbox('Pilih Kabupaten/Kota:', df['REGION'].unique())
-    future_years = st.number_input('Masukkan Jumlah Tahun yang Ingin Diprediksi:', min_value=1, max_value=20, value=3)
+# Input
+selected_region = st.selectbox('Pilih Kabupaten/Kota:', df['REGION'].unique())
+future_years = st.number_input('Masukkan Jumlah Tahun yang Ingin Diprediksi:', min_value=1, max_value=20, value=3)
 
-    # Prediction based on user input
-    if st.button('Prediksi Berdasarkan Kabupaten/Kota'):
-        future_years_list, predicted_salaries = predict_salary_increase_sarima(df, selected_region, future_years)
+# Prediction based on user input
+if st.button('Prediksi Berdasarkan Kabupaten/Kota'):
+    future_years_list, predicted_salaries = predict_salary_increase_sarima(df, selected_region, future_years)
 
-        st.write(f'Prediksi Besarnya UMR di {selected_region} untuk {future_years} tahun ke depan:')
-        prediction_df = pd.DataFrame({'Tahun': future_years_list, 'Prediksi UMR': predicted_salaries})
-        # Konversi kolom 'Tahun' menjadi string
-        prediction_df['Tahun'] = prediction_df['Tahun'].dt.strftime('%Y')
+    st.write(f'Prediksi Besarnya UMR di {selected_region} untuk {future_years} tahun ke depan:')
+    prediction_df = pd.DataFrame({'Tahun': future_years_list, 'Prediksi UMR': predicted_salaries})
+    # Konversi kolom 'Tahun' menjadi string
+    prediction_df['Tahun'] = prediction_df['Tahun'].dt.strftime('%Y')
 
-        st.write(prediction_df)
+    st.write(prediction_df)
 
-        # Visualization
-        fig, ax = plt.subplots(figsize=(8, 5))
-        ax.plot(future_years_list, predicted_salaries, marker='o')
-        ax.set_title(f'Prediksi Besarnya UMR di {selected_region}')
-        ax.set_xlabel('Tahun')
-        ax.set_ylabel('Prediksi UMR')
-        st.pyplot(fig)
+    # Visualization
+    fig, ax = plt.subplots(figsize=(8, 5))
+    ax.plot(future_years_list, predicted_salaries, marker='o')
+    ax.set_title(f'Prediksi Besarnya UMR di {selected_region}')
+    ax.set_xlabel('Tahun')
+    ax.set_ylabel('Prediksi UMR')
+    st.pyplot(fig)
 
-    # Prediction for 'INDONESIA' region
-    if st.button('Prediksi UMR Rata-Rata INDONESIA'):
-        future_years_list, predicted_salaries = predict_salary_increase_sarima_indonesia(df, future_years)
+# Prediction for 'INDONESIA' region
+if st.button('Prediksi UMR Rata-Rata INDONESIA'):
+    future_years_list, predicted_salaries = predict_salary_increase_sarima_indonesia(df, future_years)
 
-        st.write(f'Prediksi besarnya UMR Rata-Rata INDONESIA untuk {future_years} tahun ke depan')
-        prediction_df = pd.DataFrame({'Tahun': future_years_list, 'Prediksi UMR Rata-Rata': predicted_salaries})
-        # Konversi kolom 'Tahun' menjadi string
-        prediction_df['Tahun'] = prediction_df['Tahun'].dt.strftime('%Y')
-        
-        st.write(prediction_df)
+    st.write(f'Prediksi besarnya UMR Rata-Rata INDONESIA untuk {future_years} tahun ke depan')
+    prediction_df = pd.DataFrame({'Tahun': future_years_list, 'Prediksi UMR Rata-Rata': predicted_salaries})
+    # Konversi kolom 'Tahun' menjadi string
+    prediction_df['Tahun'] = prediction_df['Tahun'].dt.strftime('%Y')
+    
+    st.write(prediction_df)
 
-        # Visualization
-        fig, ax = plt.subplots(figsize=(8, 5))
-        ax.plot(future_years_list, predicted_salaries, marker='o')
-        ax.set_title('Prediksi Besarnya UMR Rata-Rata INDONESIA')
-        ax.set_xlabel('Tahun')
-        ax.set_ylabel('Prediksi UMR Rata-Rata')
-        st.pyplot(fig)
+    # Visualization
+    fig, ax = plt.subplots(figsize=(8, 5))
+    ax.plot(future_years_list, predicted_salaries, marker='o')
+    ax.set_title('Prediksi Besarnya UMR Rata-Rata INDONESIA')
+    ax.set_xlabel('Tahun')
+    ax.set_ylabel('Prediksi UMR Rata-Rata')
+    st.pyplot(fig)
